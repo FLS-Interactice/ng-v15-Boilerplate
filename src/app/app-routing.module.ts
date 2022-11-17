4d855e0a43f8a212801045b840fa6d71862576eb
Mark Handy <mjhandy@outlook.com>
2022-11-17 12:03:35 -0400

    nav with routing
    * Some basic pages, including a 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './components/pages/about-us/about-us.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent},
+  { path: 'about-us', component: AboutUsComponent},
+
+
+
+  // Wild card route for 404 page
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,22 @@ import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './components/header/header/header.component';
 import { MainNavComponent } from './components/header/main-nav/main-nav.component';
+import { GlobalSearchComponent } from './components/header/global-search/global-search.component';
+import { SiteLogoComponent } from './components/header/site-logo/site-logo.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
+import { AboutUsComponent } from './components/pages/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainNavComponent
+    MainNavComponent,
+    GlobalSearchComponent,
+    SiteLogoComponent,
+    HomePageComponent,
+    PageNotFoundComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
